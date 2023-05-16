523e6d10f65e0e68271be5c458f01bad8fc9abdd
dabarshi <70447223+dabarshi@users.noreply.github.com>
2023-05-17 01:38:17 +0600

    added a sweet alert on addCoffee and loaded data on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import { useLoaderData } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
+  const coffeeCollection = useLoaderData();
 
   return (
     <>
-      <h1 className='text-6xl text-purple-600'>Vite + React</h1>
+      <h1 className='text-6xl text-purple-600'>Our Coffee Collection : {coffeeCollection.length}</h1>
     </>
   )
 }
--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,4 +1,4 @@
-
+import Swal from 'sweetalert2';
 
 const AddCoffee = () => {
 
@@ -30,6 +30,14 @@ const AddCoffee = () => {
         .then(res => res.json())
         .then(data => {
             console.log(data)
+            if(data.insertedId){
+                Swal.fire({
+                    title: 'Success!',
+                    text: 'New coffee added successfully',
+                    icon: 'success',
+                    confirmButtonText: 'Cool'
+                  })
+            }
         })
     }
 
